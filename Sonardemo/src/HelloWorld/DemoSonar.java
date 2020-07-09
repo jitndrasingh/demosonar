0c9deb8d3f1b1c8a3ef5057680a483e861dcef6d
jitndrasingh <j.s.r.p.vingarla@gmail.com>
2020-07-09 15:49:48 +0530

    hiii

---
+++ Sonardemo/src/HelloWorld/DemoSonar.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+public class DemoSonar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Demo Testing");
+	}
+
+}
