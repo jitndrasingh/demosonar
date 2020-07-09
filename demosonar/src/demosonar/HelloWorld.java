9342880a9af7e38d9261f7e434f94605504d183b
jitndrasingh <j.s.r.p.vingarla@gmail.com>
2020-07-09 15:39:54 +0530

    Commited

---
+++ demosonar/src/demosonar/HelloWorld.java
@@ -0,0 +1,10 @@
+package demosonar;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Dear World");
+	}
+
+}
